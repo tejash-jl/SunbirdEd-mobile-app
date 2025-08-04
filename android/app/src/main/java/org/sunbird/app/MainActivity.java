1056df56250a4be30128a4c6ae26f69031e096d9
Kumar M <pkumarm@deloitte.com>
2025-08-04 11:04:38 +0530

    loacal setup

--- android/app/src/main/java/org/sunbird/app/MainActivity.java
+++ android/app/src/main/java/org/sunbird/app/MainActivity.java
@@ -1,8 +1,14 @@
-package org.sunbird;
-
-/**
- * Created by swayangjit on 16/10/19.
- */
+package org.sunbird.app; //package org.sunbird.app;
 import com.getcapacitor.BridgeActivity;
+import android.content.BroadcastReceiver;import android.content.Intent;import android.content.IntentFilter;import android.os.Build;import org.jetbrains.annotations.Nullable;import android.content.Context;
 
-public class MainActivity extends BridgeActivity {}
+
+public class MainActivity extends BridgeActivity {
+    @Override    public Intent registerReceiver(@Nullable BroadcastReceiver receiver, IntentFilter filter) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            return super.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED);
+        } else {
+            return super.registerReceiver(receiver, filter);
+        }
+    }
+}
