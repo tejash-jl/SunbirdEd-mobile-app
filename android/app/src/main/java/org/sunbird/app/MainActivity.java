aca64533dfec8c67ae79844762affc19358d510b
tejash-jl <tejash.jl1020@gmail.com>
2025-08-14 12:15:49 +0530

    refactor MainActivity.java for improved readability and formatting

--- android/app/src/main/java/org/sunbird/app/MainActivity.java
+++ android/app/src/main/java/org/sunbird/app/MainActivity.java
@@ -1,10 +1,17 @@
-package org.sunbird.app; //package org.sunbird.app;
+package org.sunbird.app;
+
 import com.getcapacitor.BridgeActivity;
-import android.content.BroadcastReceiver;import android.content.Intent;import android.content.IntentFilter;import android.os.Build;import org.jetbrains.annotations.Nullable;import android.content.Context;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import org.jetbrains.annotations.Nullable;
+import android.content.Context;
 
 
 public class MainActivity extends BridgeActivity {
-    @Override    public Intent registerReceiver(@Nullable BroadcastReceiver receiver, IntentFilter filter) {
+    @Override
+    public Intent registerReceiver(@Nullable BroadcastReceiver receiver, IntentFilter filter) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             return super.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED);
         } else {
