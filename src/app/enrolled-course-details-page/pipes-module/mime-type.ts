5ef3f27760ac74a26c0c3bf2f28f3770a5325a16
tejash-jl <tejash.jl1020@gmail.com>
2025-09-11 17:01:00 +0530

    added sdk code locally

---
+++ src/app/enrolled-course-details-page/pipes-module/mime-type.ts
@@ -0,0 +1,74 @@
+import { Pipe, PipeTransform } from '@angular/core';
+export class MimeTypeMasterData {
+  public static readonly COLLECTION = 'application/vnd.ekstep.content-collection';
+  public static readonly VIDEO = ['video/avi', 'video/mpeg', 'video/quicktime', 'video/3gpp', 'video/mpeg', 'video/mp4',
+    'video/ogg', 'video/webm'];
+  public static readonly AUDIO = ['audio/mp3', 'audio/mp4', 'audio/mpeg', 'audio/ogg', 'audio/webm', 'audio/x-wav', 'audio/wav'];
+  public static readonly INTERACTIVE = ['application/vnd.ekstep.ecml-archive', 'application/vnd.ekstep.html-archive',
+    'application/vnd.android.package-archive', 'application/vnd.ekstep.content-archive',
+    'application/vnd.ekstep.plugin-archive', 'application/vnd.ekstep.h5p-archive'];
+  public static readonly DOCS = ['application/pdf', 'application/epub', 'application/msword'];
+  public static readonly ALL = ['video/mp4', 'video/x-youtube', 'video/webm', 'application/pdf', 'application/epub',
+    'application/pdf', 'application/epub', 'application/vnd.ekstep.ecml-archive', 'application/vnd.ekstep.h5p-archive',
+    'application/vnd.ekstep.html-archive','application/vnd.sunbird.questionset', 'audio/mp3', 'audio/mp4', 'audio/mpeg', 'audio/ogg', 'audio/webm', 'audio/x-wav', 'audio/wav'
+  ];
+}
+
+/*
+  Contents are filtered based on given mimetype
+*/
+
+
+@Pipe({
+    name: 'hasMimeType',
+    standalone: false
+})
+export class MimeTypePipe implements PipeTransform {
+  transform(item?: any, mimeTypes: string[] = ['all'], isTextbookTocPage: boolean = false): boolean {
+    if (mimeTypes.indexOf('all') > -1 && item != null) {
+      if (item.mimeType !== MimeTypeMasterData.COLLECTION && !item.children) {
+        return true;
+      } else {
+        if (item.mimeType === MimeTypeMasterData.COLLECTION && item.children && !item.children.length) {
+          return false;
+        }
+        if (item.children && item.children.length) {
+          return this.getFilteredItems(item.children, MimeTypeMasterData.ALL);
+        } else {
+          return true;
+        }
+      }
+    }
+    if (item != null && item.mimeType !== MimeTypeMasterData.COLLECTION && (!item.children || !item.children.length)) {
+      return this.getFilteredItems([item], mimeTypes);
+    }
+    if(item!=null) {
+      return this.getFilteredItems(item.children, mimeTypes);
+    } else {
+      return null;
+    }
+    
+  }
+
+
+  getFilteredItems(contents, mimeTypes: string[]): boolean {
+    if (contents) {
+
+      return this.flattenDeep(contents)
+        .some((c) => !!mimeTypes.find(m => m === c.mimeType));
+    }
+  }
+
+  private flattenDeep(contents) {
+    if (contents) {
+      return contents.reduce((acc, val) => {
+        if (val.children) {
+          acc.push(val);
+          return acc.concat(this.flattenDeep(val.children));
+        } else {
+          return acc.concat(val);
+        }
+      }, []);
+    }
+  }
+}
---
+++ src/app/enrolled-course-details-page/toc-curriculum/toc-data.ts
@@ -0,0 +1,102 @@
+export const  staticData:any =  {
+  "content": {
+    "organisation": [
+      "ZPHS PALASAMUDRAM",
+      "APEKX"
+    ],
+    "children": [
+      {
+        "parent": "do_31298113330674073617",
+        "identifier": "do_3129811497340272641185",
+        "lastStatusChangedOn": "2020-03-19T10:16:44.642+0000",
+        "code": "do_3129811497340272641185",
+        "keywords": [
+          "biology",
+          "APBSTF"
+        ],
+        "visibility": "Parent",
+        "purpose": "LEARN THE NEW PATTERN ONE WORD ANSWER QUESTIONS",
+        "description": "It includes the bit bank for all the lessons of biology in 10th class",
+        "index": 1,
+        "mimeType": "application/vnd.ekstep.content-collection",
+        "createdOn": "2020-03-19T10:16:44.642+0000",
+        "versionKey": "1584613004642",
+        "framework": "ap_k-12_1",
+        "depth": 1,
+        "children": [
+          {
+            "parent": "do_3129811497340272641185",
+            "identifier": "do_3129811497340272641186",
+            "code": "do_3129811497340272641186",
+            "visibility": "Parent",
+            "description": "BIT BANK OF NUTRITION",
+            "index": 1,
+            "depth": 2,
+            "children": [
+              {
+               
+                "subject": [
+                  "Biology"
+                ],
+                "mimeType": "application/pdf",
+                "appIcon": "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/do_312957752291999744156/artifact/apbstf-logo_1581756068016.thumb.jpg",
+                "contentType": "Resource",
+                "lastUpdatedBy": "1d75fc50-792d-4e0f-a1a3-e967768c6df2",
+                "identifier": "do_312957752291999744156",
+                "audience": [
+                  "Learner"
+                ],
+                "visibility": "Default",
+                "name": "APBSTF_BIT BANK_EM_1.NUTRITION",
+                "topic": [
+                  "Nutrition In Plants, Nutrition In Animals"
+                ],
+                "creator": "KIRIMANJESWARA MANJULA",
+              }
+            ],
+            "name": "NUTRITION",
+            "lastUpdatedOn": "2020-03-20T16:15:13.472+0000",
+            "dialcodeRequired": "No",
+            "contentType": "TextBookUnit",
+            "status": "Live",
+            "compatibilityLevel": 1,
+            "lastPublishedOn": "2020-03-20T16:15:13.673+0000",
+            "pkgVersion": 1,
+            "leafNodesCount": 1
+            }
+        ],
+        "name": "APBSTF_BIT BANK_EM",
+        "lastUpdatedOn": "2020-03-20T16:15:13.472+0000",
+        "contentType": "TextBookUnit",
+        "dialcodeRequired": "No",
+        "status": "Live",
+        "compatibilityLevel": 1,
+        "lastPublishedOn": "2020-03-20T16:15:13.673+0000",
+        "pkgVersion": 1,
+        "leafNodesCount": 10,
+        "downloadUrl": "https://ntpproductionall.blob.core.windows.net/ntp-content-production/ecar_files/do_31298113330674073617/apbstf-bit-bank_1584720913805_do_31298113330674073617_1.0_spine.ecar",
+        "variants": "{\"online\":{\"ecarUrl\":\"https://ntpproductionall.blob.core.windows.net/ntp-content-production/ecar_files/do_31298113330674073617/apbstf-bit-bank_1584720914064_do_31298113330674073617_1.0_online.ecar\",\"size\":11419.0},\"spine\":{\"ecarUrl\":\"https://ntpproductionall.blob.core.windows.net/ntp-content-production/ecar_files/do_31298113330674073617/apbstf-bit-bank_1584720913805_do_31298113330674073617_1.0_spine.ecar\",\"size\":507919.0}}"
+      }
+    ],
+    "mediaType": "content",
+    "name": "APBSTF BIT BANK",
+    "subject": [
+      "Biology"
+    ],
+    "identifier": "do_31298113330674073617",
+    "description": "It is a book bank written by a group of state resource persons as per the new blue print and pattern of question paper",
+    "resourceType": "Book",
+    "gradeLevel": [
+      "Class 10"
+    ],
+    "ownershipType": [
+      "createdBy"
+    ],
+    "appIcon": "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/do_31298113330674073617/artifact/apbstf-logo_1581756068016.thumb.jpg",
+    "medium": [
+      "English"
+    ],
+    "board": "State (Andhra Pradesh)",
+    "objectType": "Content"
+  }
+};
